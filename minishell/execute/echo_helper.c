682f8eea2bb2fbd01d829593f41343473dfdd147
Selman Demir <34183418+Selmand42@users.noreply.github.com>
2024-08-18 01:55:54 +0300

    Execute klasoru olusturuldu

---
+++ minishell/execute/echo_helper.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo_helper.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: selmandemir <selmandemir@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/18 00:05:36 by selmandemir       #+#    #+#             */
+/*   Updated: 2024/08/18 01:55:19 by selmandemir      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../lib/minishel.h"
+
+void	echo_case1(char c, int *expect, int *ret)
+{
+	if (c == '-')
+		*expect = 2;
+	else
+		*ret = 0;
+}
+
+int	echo_case2(char c, int *expect, int *ret)
+{
+	if (c == 'n')
+		*expect = 3;
+	else if (c == ' ')
+	{
+		*ret = 1;
+		return (1);
+	}
+	else
+		*ret = 0;
+	return (0);
+}
+
+int	echo_case3(char c, int *expect, int *ret, int *n_count)
+{
+	if (c == ' ')
+	{
+		*expect = 1;
+		return (1);
+	}
+	else if (c == '-' && *n_count == 1)
+		*expect = 2;
+	else if (c != 'n')
+		*ret = 0;
+	return (0);
+}
+
+void	echo_incn(char c, int *n)
+{
+	if (c == 'n')
+		(*n)++;
+	else
+		*n = 0;
+}
+
+int	echo_index(char *s)
+{
+	int	ans;
+	int	expect;
+	int	n_count;
+	int	i;
+	int	ret;
+
+	i = 0;
+	ret = -1;
+	ans = 0;
+	while (i < ft_strlen(s))
+	{
+		if (expect == 1)
+			echo_case1(s[i], &expect, &ret);
+		else if (expect == 2 && echo_case2(s[i], &expect, &ret) == 1)
+			return (i + 1);
+		else if (echo_case3(s[i], &expect, &ret, &n_count) == 1)
+			ans = i + 1;
+		if (ret != -1)
+			return (ans + ret);
+		echo_incn(s[i], &n_count);
+		i++;
+	}
+	return (ans);
+}
---
+++ minishell/execute/echo_main.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo_main.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: selmandemir <selmandemir@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/18 01:19:08 by selmandemir       #+#    #+#             */
+/*   Updated: 2024/08/18 01:55:25 by selmandemir      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../lib/minishel.h"
+
+void	echo_case1(char c, int *expect, int *ret);
+int	echo_case2(char c, int *expect, int *ret);
+int	echo_case3(char c, int *expect, int *ret, int *n_count);
+void	echo_incn(char c, int *n);
+int	echo_index(char *s);
+
+int	echo_main(char *s)
+{
+	int	ind;
+
+	ind = echo_index(s);
+	printf("%s", &s[ind]);
+	if (ind < 3)
+		printf("\n");
+	return (1);
+}
+
+
+int main()
+{
+	printf("");
+	echo_main("--nnnnnn selman selman");
+	echo_main("-nnnnnn selman selman");
+}
