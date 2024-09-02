a7fd9cb90d39a1e12fab24f7abd87895bcf78018
İzzettin Furkan Özmen <izzettinfurkan.ozmen@stu.fsm.edu.tr>
2024-09-02 19:35:23 +0300

    Update pipe_unset.c

--- minishell/execute/pipe_unset.c
+++ minishell/execute/pipe_unset.c
@@ -14,14 +14,35 @@ int is_valid_unset(const char *str)
 	return 1;
 }
 
-void ft_unset(char **env, char *args[])
+char **remove_env_var(char **env, int index)
 {
-	if (args[1] == NULL)
-	{
-		return;
-	}
+	int env_size = 0;
+	while (env[env_size] != NULL)
+		env_size++;
 
-	for (int i = 1; args[i] != NULL; i++)
+	char **new_env = (char **)malloc(sizeof(char *) * env_size);
+	if (!new_env)
+		return env;
+
+	for (int k = 0, m = 0; env[k] != NULL; k++)
+	{
+		if (k != index)
+		{
+			new_env[m] = env[k];
+			m++;
+		}
+	}
+	new_env[env_size - 1] = NULL;
+
+	free(env[index]);
+	free(env);
+
+	return new_env;
+}
+
+void ft_unset(char **env, char **args)
+{
+	for (int i = 0; args[i] != NULL; i++)
 	{
 		if (!is_valid_unset(args[i]))
 		{
@@ -29,21 +50,13 @@ void ft_unset(char **env, char *args[])
 			continue;
 		}
 
-		int j = 0;
-		while (env[j] != NULL)
+		for (int j = 0; env[j] != NULL; j++)
 		{
 			if (ft_strncmp(env[j], args[i], ft_strlen(args[i])) == 0 && env[j][ft_strlen(args[i])] == '=')
 			{
-				free(env[j]);
-
-				for (int k = j; env[k] != NULL; k++)
-				{
-					env[k] = env[k + 1];
-				}
-
+				env = remove_env_var(env, j);
 				break;
 			}
-			j++;
 		}
 	}
 }
