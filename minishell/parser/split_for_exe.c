a9d6eba6ce4a1671f490026ea335c60b42dfec70
usisman <166256240+usisman@users.noreply.github.com>
2024-09-02 18:20:38 +0300

    split for exe değişti sadece heredoc ve less kaldı

--- minishell/parser/split_for_exe.c
+++ minishell/parser/split_for_exe.c
@@ -1,14 +1,6 @@
 #include "../minishell.h"
 
-void fd_zero(t_fd *fd)
-{
-	fd->file = -1;
-	fd->name = NULL;
-	fd->next = NULL;
-	fd->type = -1;
-}
-
-char *cmd_to_lower(char *str)
+void cmd_to_lower(char *str)
 {
 	int i;
 
@@ -16,119 +8,30 @@ char *cmd_to_lower(char *str)
 	while(str[i])
 	{
 		if (str[i] >= 'A' && str[i] <= 'Z')
-		{
 			str[i] += 32;
-		}
 		i++;
 	}
-	return (str);
-}
-
-void line_zero(t_line *line)
-{
-	line->arg = NULL;
-	line->cmd = NULL;
-	line->fd = NULL;
-	line->next = NULL;
-}
-
-void start_new_list_fd(t_line *line)
-{
-	t_fd *a;
-    a = (t_fd *)malloc(sizeof(t_fd)); // Hatalı olan kısım düzeltilmiş
-    if (a == NULL) {
-        perror("malloc failed");
-        exit(EXIT_FAILURE);
-    }
-    line->fd = a;
-    fd_zero(line->fd);
-}
-
-void start_next_list_line(t_line **line) // neden cift pointer alıyor bilmiyorum??!!
-{
-    *line = (t_line *)malloc(sizeof(t_line)); // Hatalı olan kısım düzeltilmiş
-	if(!line)
-	{
-		// hata kontrolu
-	}
-    line_zero(*line);
-}
-
-void add_next_list_fd(t_fd *fd)
-{
-	t_fd *tmp;
-	tmp = malloc(sizeof(t_fd));
-	if(!tmp)
-	{
-		// hata kontrolu
-	}
-	fd_zero(tmp);
-	fd->next = tmp;
-}
-// liste oluşturmalar güncellenicek.
-void add_new_list_line(t_line *line)
-{
-	t_line *tmp;
-	tmp = malloc(sizeof(t_line));
-	if(!tmp)
-	{
-		// hata kontrolu
-	}
-	line_zero(tmp);
-	line->next = tmp;
-}
-
-void line_list_file(t_split *tmp, t_line *line)
-{
-		line->fd->name = tmp->node;
-		line->fd->type = tmp->meta;
-		add_next_list_fd(line->fd); 
-		line->fd = line->fd->next;
-		tmp = tmp->next;
-}
-
-int ft_args_len(t_split *split)
-{
-	t_split *tmp;
-	tmp = split;
-	int i;
-	i = 0;
-	while(tmp && tmp->meta != PIPE && is_redir(tmp))
-	{
-		while(tmp->node[i])
-			i++;
-		if(tmp->next != NULL)
-			tmp = tmp->next;
-		else
-			break;
-	}
-	return (i);
-}
-
-void line_list_arg(t_split *tmp, t_line *line) // ABORT
-{
-	int i;
-
-	i = 0;
-	line->arg = malloc(ft_args_len(tmp) * sizeof(char **)); // burda arg sayısı kadar yer acmak gerkli burası cokomelli !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!?????????????????????????????????????????
-	while(tmp && tmp->meta != PIPE && is_redir(tmp)) // echo adsadsa >> b.txt de adsadsa diye dosya açmıyo o yüzden echo özel durum redir ya da pipe görene kadar argüman olarak alsın SOLVED ACCES LE DOSYAYI DENİCEZ
-	{
-		line->arg[i] = malloc(ft_strlen(tmp->node) * sizeof(char));
-		line->arg[i] = tmp->node; // strcpy??*
-		i++;
-		tmp = tmp->next;
-		// line arg icin double pointer yer acmak icin kac arguman oldugunu bilip ona yer acıp sonrasında uzunlugu kadar da acmak lazım
-	}
-	line->arg[i] = NULL;
 }
 
 int is_redir(t_split *split)
 {
-	t_split *tmp;
-	tmp = split;
-	if(tmp->meta == 1 || tmp->meta == 3 || tmp->meta == 4 || tmp->meta == 5)
+	if(split->meta == HEREDOC || split->meta == GREAT || split->meta == GREATER || split->meta == LESS)
 		return (1);
 	return 0;
+}
+
+void line_list_file(t_split *tmp, t_line *line)
+{
+	append_fd(&(line->fd), create_new_fd(tmp->node, tmp->meta));
+}
+
+void line_list_arg(t_split *tmp, t_line *line)
+{
+	while(tmp && tmp->meta != PIPE && is_redir(tmp))
+	{
+		add_arg(&(line->arg), tmp->node);
+		tmp = tmp->next;
+	}
 }
 
 t_line *split_for_exe(t_split *split, t_mini *mini) // denenmedi. // HEREDOC KOŞULLARI BİLİNMEDİĞİ İÇİN EKLENMEDİ ONLARI ÖĞRENİP GÜNCELLİCEZ.
@@ -141,24 +44,25 @@ t_line *split_for_exe(t_split *split, t_mini *mini) // denenmedi. // HEREDOC KO�
 	tmp_spl = split;
 	flag_pipe = 1;
 	line = NULL; 
-	start_next_list_line(&line);
-	start_new_list_fd(line);
+
 	tmp2 = line;
 	while(tmp_spl)
 	{
-		if(flag_pipe == 1)  // açılabiliyo mu sonraki node u redir mi check i atılcak dosyalraa
+		if(flag_pipe == 1)
 		{
 			if(tmp_spl == NULL)
 				break;
-			line->cmd = cmd_to_lower(tmp_spl->node);
+			cmd_to_lower(tmp_spl->node);
+			tmp2 = create_new_line(tmp_spl, &(mini->env));
+			append_line(&line, tmp2); // yeni liste oluşturma func_2
 			flag_pipe = 0;
 			tmp_spl = tmp_spl->next;
 			if(tmp_spl == NULL)
 				break;
 		}
-		if(tmp_spl->meta == EXCEPT && flag_pipe == 0)// && tmp->meta != PIPE) //-> bu kontrol gerekli mi
+		if(tmp_spl->meta == EXCEPT && flag_pipe == 0)
 		{
-			line_list_arg(tmp_spl, tmp2);
+			line_list_arg(tmp_spl, tmp2); // ok
 		}
 		else if(tmp_spl->meta == GREAT || tmp_spl->meta == GREATER)
 		{
@@ -169,13 +73,14 @@ t_line *split_for_exe(t_split *split, t_mini *mini) // denenmedi. // HEREDOC KO�
 		{
 			flag_pipe = 1;
 			tmp_spl = tmp_spl->next;
-			add_new_list_line(tmp2); // yeni liste oluşturma func_2
-			tmp2->env_line = mini->env;
-			tmp2 = tmp2->next;
-			start_new_list_fd(tmp2); // yeni listenin fd si başlatılcak.
 		}
 		else
+		{
+			if(tmp_spl == NULL)
+				break;
+			printf("Error, can't find next");
 			tmp_spl = tmp_spl->next;
-	} // 									 en son herşey bitince adresleri geri iade mi ediyoduk ?????????????
+		}
+	}
 	return(line);
 }
