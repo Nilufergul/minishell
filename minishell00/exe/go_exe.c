fb9db889893182bbe90e2089ca304854c4e478b8
Selman Demir <34183418+Selmand42@users.noreply.github.com>
2024-09-06 03:05:15 +0300

    Update go_exe.c
    merge_echo fonksiyonu eklendi

--- minishell00/exe/go_exe.c
+++ minishell00/exe/go_exe.c
@@ -18,3 +18,24 @@ char	*get_copy(char *result, char **arr)
 	}
 	return (cur);
 }
+
+char	*merge_echo(char **arr)
+{
+	char	*cur;
+	char	*temp;
+	int		i;
+
+	if (!arr || !arr[0])
+		return (0);
+	cur = ft_strdup(arr[0]);
+	i = 1;
+	while (arr != NULL && arr[i] != NULL)
+	{
+		temp = ft_strjoin(cur, " ");
+		free(cur);
+		cur = ft_strjoin(temp, arr[i]);
+		free(temp);
+		i++;
+	}
+	return (cur);
+}
