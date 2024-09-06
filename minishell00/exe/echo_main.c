c721d34489f5bef1aac16e18dad72c9c64a5ce9e
Selman Demir <34183418+Selmand42@users.noreply.github.com>
2024-09-06 03:16:30 +0300

    Update echo_main.c

--- minishell00/exe/echo_main.c
+++ minishell00/exe/echo_main.c
@@ -5,18 +5,14 @@ int	echo_main(char *s)
 {
 	int	ind;
 
+	if (!s)
+	{
+		printf("\n");
+		return (1);
+	}
 	ind = echo_index(s);
 	printf("%s", &s[ind]);
 	if (ind < 3)
 		printf("\n");
 	return (1);
 }
-
-/*
-int main()
-{
-	printf("");
-	echo_main("--nnnnnn selman selman");
-	echo_main("-nnnnnn selman selman");
-}
-*/
