bdf8af4eb6ab34d69290513553c921d6a13e2f39
Selman Demir <34183418+Selmand42@users.noreply.github.com>
2024-09-08 01:46:53 +0300

    Update libft.h

--- minishell00/libft/libft.h
+++ minishell00/libft/libft.h
@@ -50,7 +50,7 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-int	  ft_strcmp(char *s1, char *s2);
+int		ft_strcmp(char *s1, char *s2);
 char	*ft_strcpy(char *s1, char *s2);
 
 #endif
