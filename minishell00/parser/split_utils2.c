114117f2ed90cfb501daccb8fd5336f9f886b6b1
Selman Demir <34183418+Selmand42@users.noreply.github.com>
2024-09-08 01:55:27 +0300

    Update split_utils2.c

--- minishell00/parser/split_utils2.c
+++ minishell00/parser/split_utils2.c
@@ -22,3 +22,22 @@ void	line_list_arg(t_split *tmp, t_line *line)
 {
 	add_arg(&(line->arg), tmp->node);
 }
+
+void	quotes_cases(t_split **split, t_mini **mini, int *flag)
+{
+	if (closed_quotes_index((*split)->node)) // dolar tırnak grubu içinde mi kalıyor
+	{
+		if ((quoted_dollar((*split)) && !closed_quotes_index((*split)->node)))
+			replace_node_substr((*split), "$", "");
+		if (closed_quotes_index((*split)->node) == '\'') // dolar hangi tırnak içinde kalıyor (tek tırnak içindeki dolar çalışmıyor
+			*flag = 1;
+	}
+	else if (quoted_dollar((*split)))
+		replace_node_substr((*split), "$", "");
+	if (*flag == 0)
+	{
+		dollar_quest((*split)); // $? çalıştırıyor
+		handle_dollar((*split), *mini); // expand edilicek dolarları çalıştırıyor.
+	}
+	(*split)->meta = EXCEPT;
+}
