abe8c9a06c35e4572eec714565a22be5c539824a
Selman Demir <34183418+Selmand42@users.noreply.github.com>
2024-09-08 01:44:32 +0300

    Update ft_strcpy.c

--- minishell00/libft/ft_strcpy.c
+++ minishell00/libft/ft_strcpy.c
@@ -2,14 +2,14 @@
 
 char	*ft_strcpy(char *s1, char *s2)
 {
-	  int i;
+	int	i;
 
-	  i = 0;
-	  while (s2[i])
-	  {
-		  s1[i] = s2[i];
-		  i++;
-	  }
-	  s1[i] = s2[i];
-	  return (s1);
-  }
+	i = 0;
+	while (s2[i])
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	s1[i] = s2[i];
+	return (s1);
+}
