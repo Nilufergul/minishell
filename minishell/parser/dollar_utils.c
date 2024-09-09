ee27127f6eee91a7b67f231978c9ef8e40eb6089
Nilufergul <142345019+Nilufergul@users.noreply.github.com>
2024-09-09 18:20:03 +0300

    hatalı ama olsun (echo '$PATH'$PATH için deneme)
    :(

--- minishell/parser/dollar_utils.c
+++ minishell/parser/dollar_utils.c
@@ -1,99 +1,144 @@
 #include "../minishell.h"
 
-int count_substr(const char *str, const char *sub)
+int	count_substr(const char *str, const char *sub)
 {
-    int count;
-    const char *tmp;
-    int sub_len;
+	int			count;
+	const char	*tmp;
+	int			sub_len;
 
 	count = 0;
 	tmp = str;
 	sub_len = ft_strlen(sub);
-    
-    while ((tmp = ft_strnstr(tmp, sub , ft_strlen(tmp))) != NULL) {
-        count++;
-        tmp += sub_len;
-    }
-	free((void*)tmp);
-    return count;
+	tmp = ft_strnstr(tmp, sub, ft_strlen(tmp));
+	while (tmp != NULL)
+	{
+		count++;
+		tmp += sub_len;
+		tmp = ft_strnstr(tmp, sub, ft_strlen(tmp));
+	}
+	free((void *)tmp);
+	return (count);
 }
 
-char *allocate_new_node(const char *node, const char *var, const char *value)
+char	*allocate_new_node(const char *node, const char *var, const char *value)
 {
-    int old_len;
-    int new_len;
-    int extra_len;
-	char *new_node;
+	int		old_len;
+	int		new_len;
+	int		extra_len;
+	char	*new_node;
 
 	old_len = strlen(var);
 	new_len = strlen(value);
 	extra_len = 0;
-
-    if (new_len > old_len)
+	if (new_len > old_len)
 	{
-        extra_len = (new_len - old_len) * count_substr(node, var);
-    }
-	
+		extra_len = (new_len - old_len) * count_substr(node, var);
+	}
 	new_node = malloc(strlen(node) + extra_len + 1);
-    return new_node;
+	if (!new_node)  // Bellek tahsisi başarısız olduysa
+		return (NULL);
+	return (new_node);
 }
-
-
-void replace_substr_in_node(char *new_node, const char *node, const char *var, const char *value) 
+char	*ft_strnstr1(const char *h, const char *ndl, size_t n)
 {
-    const char *str;
-    char *pos;
-    int old_len;
-    int new_len;
-
-    str = node;
-    pos = new_node;
-    old_len = strlen(var);
-    new_len = strlen(value);
-    while (*str) 
-    {
-        if (ft_strnstr(str, var, ft_strlen(str)) == str) 
-        {
-            ft_strlcpy(pos, value, new_len + 1);
-            pos += new_len;
-            str += old_len;
-        } 
-        else 
-        {
-            *pos++ = *str++;
-        }
-    }
-    *pos = '\0';
-}
-
-
-void replace_node_substr(t_split *node_struct, const char *var, const char *value)
-{
-    if (!node_struct || !node_struct->node || !var || !value)
-        return;
-
-    char *new_node = allocate_new_node(node_struct->node, var, value);
-    if (!new_node)
-        return;
-
-    replace_substr_in_node(new_node, node_struct->node, var, value);
-
-    free(node_struct->node);
-    node_struct->node = new_node;
-}
-
-int 	quoted_dollar(t_split *split)
-{
-    int i;
-    
-    i = 0;
-    while (split->node[i] != '\0') 
+	size_t	i;
+	size_t	j;
+	i = 0;
+	j = 0;
+	if (ndl[0] == '\0')
+		return ((char *)h);
+	while (h[i] != '\0' && i < n)
 	{
-        if ((split->node[i] == '$') && (split->node[i + 1] == '\"' || split->node[i + 1] == '\''))
-        {
-           return 1;
-        }
-        i++;
-    }
-	return 0;
+		if(h[i] == '\'')
+		{
+			i++;
+			while(h[i] != '\'')
+			{
+				i++;
+			}
+		}
+		if (h[i] == ndl[j])
+		{
+			while (h[i + j] == ndl[j] && i + j < n)
+			{
+				if (ndl[j + 1] == '\0')
+					return ((char *)h + i);
+				j++;
+			}
+			j = 0;
+		}
+		i++;
+	}
+	return (NULL);
 }
+
+
+void	replace_substr_in_node(char *new_node,
+						const char *node, const char *var, const char *value)
+{
+	const char	*str;
+	char		*pos;
+	int			old_len;
+	int			new_len;
+	int			in_single_quote;
+
+	str = node;
+	pos = new_node;
+	old_len = strlen(var);
+	new_len = strlen(value);
+	in_single_quote = 0;
+
+	while (*str)
+	{
+		// Tek tırnak içinde olup olmadığımızı kontrol ederiz
+		if (*str == '\'')
+			in_single_quote = !in_single_quote;  // Tek tırnak flag'ini değiştir
+
+		// Eğer tek tırnak içinde değilsek, arama yap
+		if (!in_single_quote && ft_strnstr1(str, var, ft_strlen(str)) == str)
+		{
+			ft_strlcpy(pos, value, new_len + 1);
+			pos += new_len;
+			str += old_len;
+		}
+		else
+		{
+			*pos++ = *str++;
+		}
+	}
+	*pos = '\0';
+}
+
+
+
+
+void	replace_node_substr(t_split *node, const char *var, const char *value)
+{
+	char	*new_node;
+	if (!node || !node->node || !var || !value)
+		return ;
+	new_node = allocate_new_node(node->node, var, value);
+	if (!new_node)
+		return ;
+	replace_substr_in_node(new_node, node->node, var, value);
+	free(node->node);
+	node->node = new_node;
+}
+
+int	quoted_dollar(t_split *split)
+{
+	int	i;
+
+	i = 0;
+	while (split->node[i] != '\0')
+	{
+		if ((split->node[i] == '$') && (split->node[i + 1] == '\"'
+				|| split->node[i + 1] == '\''))
+		{
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
--- minishell/parser/removing_quotes.c
+++ minishell/parser/removing_quotes.c
@@ -1,24 +1,21 @@
 #include "../minishell.h"
-#include "../minishell.h"
 
-void quotes(t_split *split)
+void	quotes(t_split *split)
 {
-	int i;
-	int j;
-	char quote;
+	int		i;
+	int		j;
+	char	quote;
 
 	i = 0;
 	while (split->node[i])
-	{	
+	{
 		if (split->node[i] == '\"' || split->node[i] == '\'')
 		{
 			quote = split->node[i];
 			j = i;
 			i++;
 			while (split->node[i] && split->node[i] != quote)
-			{
 				i++;
-			}
 			if (split->node[i] == quote)
 			{
 				char_remove(split, j);
@@ -27,25 +24,22 @@ void quotes(t_split *split)
 			}
 		}
 		else
-		{
 			i++;
-		}
 	}
 }
 
-void char_remove(t_split *split, int i)
+void	char_remove(t_split *split, int i)
 {
-	int len;
+	int	len;
 
 	len = ft_strlen(split->node);
 	if (i < 0 || i >= len)
-		return;
-
+		return ;
 	ft_memmove(split->node + i, split->node + i + 1, len - i);
 	split->node[len - 1] = '\0';
 }
 
-void remove_quotes(t_split *split)
+void	remove_quotes(t_split *split)
 {
 	while (split)
 	{
@@ -54,53 +48,33 @@ void remove_quotes(t_split *split)
 	}
 }
 
-void dollar_quest(t_split *split)
+void	dollar_quest(t_split *split)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (split->node[i] != '\0') 
+	i = 0;
+	while (split->node[i] != '\0')
 	{
-        if ((split->node[i] == '$') && (split->node[i + 1] == '?') && !(ft_isalnum(split->node[i + 2])))
-        {
-			printf("dolar soru işareti çalışcak.exit status\n");
-			replace_node_substr(split,"$?", "mock_exitstatus");
-            //dolar soru işareti çalışcak.exit status
-        }
-        i++;
+		if ((split->node[i] == '$') && (split->node[i + 1] == '?')
+			&& !(ft_isalnum(split->node[i + 2])))
+		{
+			printf("dolar soru işareti çalışcak.exit status\n"); //?????!!!!!!!!!!
+			replace_node_substr(split, "$?", "mock_exitstatus"); //?????!!!!!!!!!!
+		}
+		i++;
 	}
-    
 }
 
-void expander(t_split *split, t_mini *mini)  // $'PAT'   kontroo edilcek
+void	expander(t_split *split, t_mini *mini)// $'PAT'   kontrol edilcek // dolar kontrolü burada başlıyor.
 {
-	int flag;
+	int	flag;
 
 	flag = 0;
-	while(split)
+	while (split)
 	{
-		if(split->meta == DOLLAR)
-		{
-			if(closed_quotes_index(split->node)) // dolar tırnak grubu içinde mi kalıyor
-			{
-				if(quoted_dollar(split))
-				{
-					replace_node_substr(split,"$", "");
-				}
-				if(closed_quotes_index(split->node) == '\'')
-				{
-					flag = 1;
-				}
-			}
-			if(flag == 0)
-			{
-				dollar_quest(split);
-				handle_dollar(split, mini);
-			}
-		}
+		if (split->meta == DOLLAR)
+			quotes_cases(&split, &mini, &flag);
 		flag = 0;
 		split = split->next;
 	}
 }
-
-
