2182f388f7f71e682d13ee96c70863133e516e0a
Selman Demir <34183418+Selmand42@users.noreply.github.com>
2024-09-12 17:19:55 +0300

    Update make_pipe.c
    exit kodlari icin bolca degisiklik

--- minishell00/exe/make_pipe.c
+++ minishell00/exe/make_pipe.c
@@ -1,9 +1,9 @@
 
 #include "../minishell.h"
 
-void	free_pipe_things(t_pipe_info *pipe_info)
+void	free_pipe_things(t_line *command, t_pipe_info *pipe_info)
 {
-	clean_pipes(pipe_info);
+	clean_pipes(command, pipe_info);
 	free(pipe_info->pid);
 	free_all_pipes(pipe_info->pipes, pipe_info->len - 1);
 	free(pipe_info->pipes);
@@ -13,8 +13,8 @@ void	free_pipe_things(t_pipe_info *pipe_info)
 void	make_pipe(t_line *command)
 {
 	t_pipe_info	*pipe_info;
-	
-	if (struct_len(command) == 1 && built_in2(command))
+
+	if ((command->cmd[0] == 0) || (struct_len(command) == 1 && built_in2(command)))
 		return ;
 	pipe_info = malloc(sizeof(t_pipe_info));
 	if (pipe_info == NULL)
@@ -25,21 +25,20 @@ void	make_pipe(t_line *command)
 		exit(1);
 	pipe_info->pipes = fill_pipes(pipe_info->len);
 	create_processes(command, pipe_info);
-	free_pipe_things(pipe_info);
+	free_pipe_things(command, pipe_info);
+
 }
 
 int	built_in2(t_line *command)
 {
-	if (command->cmd == NULL)
-		return (0);
 	if (ft_strcmp(command->cmd, "cd") == 0)
-		*command->exit_code_line = ft_cd(command);
+		command->exit_code_line = ft_cd(command);
 	else if (ft_strcmp(command->cmd, "export") == 0)
-		*command->exit_code_line = ft_export(command);
+		command->exit_code_line = ft_export(command);
 	else if (ft_strcmp(command->cmd, "unset") == 0)
 		ft_unset(command);
 	else if (ft_strcmp(command->cmd, "env") == 0)
-		*command->exit_code_line = ft_environment(*(command->env));
+		command->exit_code_line = ft_environment(*(command->env));
 	else if (ft_strcmp(command->cmd, "exit") == 0)
 		ft_exit(command);
 	else
@@ -47,7 +46,7 @@ int	built_in2(t_line *command)
 	return (1);
 }
 
-void	clean_pipes(t_pipe_info *pipe_info)
+void	clean_pipes(t_line *command, t_pipe_info *pipe_info)
 {
 	int	i;
 
@@ -61,7 +60,13 @@ void	clean_pipes(t_pipe_info *pipe_info)
 	i = 0;
 	while (i < pipe_info->len)
 	{
-		waitpid(pipe_info->pid[i], NULL, 0);
+		waitpid(pipe_info->pid[i], &command->exit_code_line, 0);
+		if (ft_strcmp(command->env[0][0], "?=0") == 0)
+		{
+			command->exit_code_line = w_exit_status(command->exit_code_line);
+			free(command->env[0][0]);
+			command->env[0][0] = ft_strjoin(ft_strdup("?="), ft_strdup(ft_itoa(command->exit_code_line)));
+		}
 		i++;
 	}
 }
