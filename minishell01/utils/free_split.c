e5101c6c192e67ca80e746dd6fd32fa67d15c360
Doğu Arıkan <118825959+doguarikan@users.noreply.github.com>
2024-09-15 18:23:50 +0300

    norminette fixed

--- minishell01/utils/free_split.c
+++ minishell01/utils/free_split.c
@@ -19,29 +19,29 @@ void	free_the_minis(t_mini *mini)
 	free(mini->line);
 }
 
-void free_and_cpy_env(t_line *line) 
+void	free_and_cpy_env(t_line *line)
 {
-    int i = 0;
-    char **tmp;
+	int		i;
+	char	**tmp;
 
-    while (line->env[i])
-        i++;
-
-    tmp = (char **)malloc((i + 1) * sizeof(char *));
-    if (!tmp) {
-    }
-    i = 0;
-    while (line->env[i]) 
+	i = 0;
+	while (line->env[i])
+		i++;
+	tmp = (char **)malloc((i + 1) * sizeof(char *));
+	if (!tmp)
 	{
-        tmp[i] = *line->env[i]; 
-        i++;
-    }
-    tmp[i] = NULL;
-
-    i = 0;
-    while (line->env[i]) {
-        free(line->env[i]);
-        i++;
-    }
-   
+	}
+	i = 0;
+	while (line->env[i])
+	{
+		tmp[i] = *line->env[i];
+		i++;
+	}
+	tmp[i] = NULL;
+	i = 0;
+	while (line->env[i])
+	{
+		free(line->env[i]);
+		i++;
+	}
 }
