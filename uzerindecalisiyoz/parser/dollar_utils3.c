8e58e98b6f2e9b44a22ab49250f762e8b007c81c
Doğu Arıkan <118825959+doguarikan@users.noreply.github.com>
2024-09-23 18:04:32 +0300

    question mark added

--- uzerindecalisiyoz/parser/dollar_utils3.c
+++ uzerindecalisiyoz/parser/dollar_utils3.c
@@ -6,36 +6,25 @@
 /*   By: ngulcift <ngulcift@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 12:03:58 by ngulcift          #+#    #+#             */
-/*   Updated: 2024/09/20 14:31:44 by ngulcift         ###   ########.fr       */
+/*   Updated: 2024/09/23 17:59:50 by ngulcift         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	get_double_quote(char *str, int i, char **new_str, t_mini **mini)
+void	get_qm(char **new_str, t_exit_status *exit_t)
 {
+	char	*res;
 	char	*temp;
 
-	while (str[i] && str[i] != '\"')
-	{
-		if (str[i] == '$' && str[i + 1]
-			&& (ft_isalnum(str[i + 1]) || str[i + 1] == '_'))
-			i = get_dollar(str, i + 1, mini, new_str);
-		else
-		{
-			temp = ft_calloc(ft_strlen(*new_str) + 2, 1);
-			ft_strcpy(temp, *new_str);
-			temp[ft_strlen(*new_str)] = str[i];
-			temp[ft_strlen(*new_str) + 1] = 0;
-			free(*new_str);
-			*new_str = temp;
-		}
-		i++;
-	}
-	return (i);
+	res = ft_itoa(exit_t->exit_code);
+	temp = ft_strjoin(*new_str, res);
+	free(*new_str);
+	*new_str = temp;
+	free(res);
 }
 
-char	*remove_quotes_two(t_split **split, t_mini **mini)
+char	*remove_quotes_two(t_split **split, t_mini **mini, t_exit_status *exit_t)
 {
 	int		i;
 	char	*new_str;
@@ -47,7 +36,13 @@ char	*remove_quotes_two(t_split **split, t_mini **mini)
 	str = (*split)->node;
 	while (str[i])
 	{
-		i = update_index(str, &new_str, i, mini);
+		if (str[i] == '$' && str[i + 1] && str[i + 1] == '?')
+		{	
+			get_qm(&new_str, exit_t);
+			i++;
+		}
+		else		
+			i = update_index(str, &new_str, i, mini);
 		i++;
 	}
 	free(str);
@@ -57,14 +52,16 @@ char	*remove_quotes_two(t_split **split, t_mini **mini)
 int	update_index(char *str, char **new_str, int i, t_mini **mini)
 {
 	char	*temp;
+	int		flag;
 
+	flag = 1;
 	if (str[i] == '$' && str[i + 1] && \
 		(ft_isalnum(str[i + 1]) || str[i + 1] == '_'))
 		i = get_dollar(str, i + 1, mini, new_str);
-	else if (str[i] == '\'')
+	else if (str[i] == '\'' && flag == 1)
 		i = get_single_quote(str, i + 1, new_str);
 	else if (str[i] == '\"')
-		i = get_double_quote(str, i + 1, new_str, mini);
+		flag ^= 1;
 	else
 	{
 		temp = ft_calloc(ft_strlen(*new_str) + 2, 1);
