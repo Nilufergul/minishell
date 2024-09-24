c9ecda02be22b652fb097a696472fa0ac2a8a960
Selman Demir <34183418+Selmand42@users.noreply.github.com>
2024-09-24 16:50:39 +0300

    fonksiyon eklendi

--- uzerindecalisiyoz/exe/go_exe.c
+++ uzerindecalisiyoz/exe/go_exe.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   go_exe.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ngulcift <ngulcift@student.42.fr>          +#+  +:+       +#+        */
+/*   By: muhademi <muhademi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 12:06:48 by ngulcift          #+#    #+#             */
-/*   Updated: 2024/09/23 21:20:27 by ngulcift         ###   ########.fr       */
+/*   Updated: 2024/09/24 16:50:18 by muhademi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-char	*get_copy(char *result, char **arr)
+char	**get_copy(char *result, char **arr)
 {
 	char	*cur;
 	char	*temp;
@@ -28,7 +28,7 @@ char	*get_copy(char *result, char **arr)
 		free(temp);
 		i++;
 	}
-	return (cur);
+	return (ft_split(cur, ' '));
 }
 
 char	*merge_echo(char **arr)
@@ -54,26 +54,45 @@ char	*merge_echo(char **arr)
 
 void	runcommanderror(char *command, int i)
 {
+	ft_putstr_fd("minishell: ", 2);
+	ft_putstr_fd(command, 2);
 	if (i == 1)
 	{
-		write(1,"minishell: ",11);
-		ft_putstr_fd(command, 2);
 		ft_putendl_fd(": No such file or directory", 2);
 		exit(127);
 	}
 	else if (i == 0)
 	{
-		write(1,"minishell: ",11);
-		ft_putstr_fd(command, 2);
 		ft_putendl_fd(": is a directory: ", 2);
 		exit(126);
 	}
 	else if (i == 2)
 	{
-		write(1,"minishell: ",11);
-		ft_putstr_fd(command, 2);
 		ft_putendl_fd(": Permission denied", 2);
 		exit(126);
 	}
+	else if (i == 3)
+	{
+		ft_putendl_fd(": There is no such command", 2);
+		exit(127);
+	}
 }
 
+int	check_file(t_line *command, int flag)
+{
+	if (flag && !ft_strncmp(command->cmd, "./", 2) && is_directory(command->cmd))
+		runcommanderror(command->cmd, 0);
+	if (flag && !ft_strncmp(command->cmd, "./", 2) && !is_file(command->cmd))
+		runcommanderror(command->cmd, 1);
+	if (flag && !ft_strncmp(command->cmd, "./", 2) && access(command->cmd, X_OK) == -1)
+		runcommanderror(command->cmd, 2);
+	if (!flag && is_directory(command->cmd))
+		runcommanderror(command->cmd, 3);
+	if (!flag && !is_file(command->cmd))
+		runcommanderror(command->cmd, 3);
+	if (!flag && ft_strncmp(command->cmd, "./", 2) && command->cmd[0] != '/')
+		runcommanderror(command->cmd, 3);
+	if (!flag && access(command->cmd, X_OK) == -1)
+		runcommanderror(command->cmd, 3);
+	return (1);
+}
