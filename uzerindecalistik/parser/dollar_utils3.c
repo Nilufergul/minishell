2ac8322cd8259c4f2c96838426903d42ad85e057
Selman Demir <34183418+Selmand42@users.noreply.github.com>
2024-09-27 01:46:40 +0300

    flag hatasi cozuldu

--- uzerindecalistik/parser/dollar_utils3.c
+++ uzerindecalistik/parser/dollar_utils3.c
@@ -27,51 +27,49 @@ void	get_qm(char **new_str, t_exit_status *exit_t)
 char	*remove_quotes_two(t_split **split, t_mini **mini, \
 	t_exit_status *exit_t)
 {
-	int		i;
+	int		nums[2];
 	char	*new_str;
 	char	*str;
 
-	i = 0;
-	new_str = malloc(1);
-	new_str[0] = 0;
+	nums[0] = 0;
+	nums[1] = 1;
+	new_str = ft_calloc(1, 1);
 	str = (*split)->node;
-	while (str[i])
+	while (str[nums[0]])
 	{
-		if (str[i] == '$' && str[i + 1] && str[i + 1] == '?')
+		if (str[nums[0]] == '$' && str[nums[0] + 1] && str[nums[0] + 1] == '?')
 		{
 			get_qm(&new_str, exit_t);
-			i++;
+			nums[0]++;
 		}
 		else
-			i = update_index(str, &new_str, i, mini);
-		i++;
+			nums[0] = update_index(str, &new_str, nums, mini);
+		nums[0]++;
 	}
 	free(str);
 	return (new_str);
 }
 
-int	update_index(char *str, char **new_str, int i, t_mini **mini)
+int	update_index(char *str, char **new_str, int nums[2], t_mini **mini)
 {
 	char	*temp;
-	int		flag;
 
-	flag = 1;
-	if (str[i] == '$' && str[i + 1] && \
-		(ft_isalnum(str[i + 1]) || str[i + 1] == '_'))
-		i = get_dollar(str, i + 1, mini, new_str);
-	else if (str[i] == '\'' && flag == 1)
-		i = get_single_quote(str, i + 1, new_str);
-	else if (str[i] == '\"')
-		flag ^= 1;
+	if (str[nums[0]] == '$' && str[nums[0] + 1] && \
+		(ft_isalnum(str[nums[0] + 1]) || str[nums[0] + 1] == '_'))
+		nums[0] = get_dollar(str, nums[0] + 1, mini, new_str);
+	else if (str[nums[0]] == '\'' && nums[1] == 1)
+		nums[0] = get_single_quote(str, nums[0] + 1, new_str);
+	else if (str[nums[0]] == '\"')
+		nums[1] ^= 1;
 	else
 	{
 		temp = ft_calloc(ft_strlen(*new_str) + 2, 1);
 		ft_strcpy(temp, *new_str);
-		temp[ft_strlen(*new_str)] = str[i];
+		temp[ft_strlen(*new_str)] = str[nums[0]];
 		temp[ft_strlen(*new_str) + 1] = 0;
 		free(*new_str);
 		*new_str = ft_strdup(temp);
 		free(temp);
 	}
-	return (i);
+	return (nums[0]);
 }
