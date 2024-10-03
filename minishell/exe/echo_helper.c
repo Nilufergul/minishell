417bd03b37dd3e3e50c3dfbef591f673125225df
Selman Demir <34183418+Selmand42@users.noreply.github.com>
2024-10-03 12:54:32 +0300

    append echo_main function

--- minishell/exe/echo_helper.c
+++ minishell/exe/echo_helper.c
@@ -72,3 +72,19 @@ int	echo_index(char *s)
 		ans += 3;
 	return (ans);
 }
+
+int	echo_main(char *s)
+{
+	int	ind;
+
+	if (!s)
+	{
+		printf("\n");
+		return (0);
+	}
+	ind = echo_index(s);
+	printf("%s", &s[ind]);
+	if (ind < 3)
+		printf("\n");
+	return (0);
+}
